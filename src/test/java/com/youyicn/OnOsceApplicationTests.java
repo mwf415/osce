c3c33ce2be75161f87778d4e6fa792ecc7f9efbc
mwf415 <mwf415@126.com>
2019-07-19 16:42:28 +0800

    haha

--- src/test/java/com/youyicn/OnOsceApplicationTests.java
+++ src/test/java/com/youyicn/OnOsceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootShiroApplicationTests {
+public class OnOsceApplicationTests {
 
 	@Test
 	public void contextLoads() {
